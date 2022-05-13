6f598731c76b5944903f8b607b154b3a8a68bee2
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-05-13 19:18:30 -0300

    Revertendo

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,11 +1,12 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { BuscarPokemons } from "./Pages/BuscarPokemons";
 
 export const AppRoute = () => {
   return(
-    <Router>
+    <HashRouter>
+      <Routes>
         <Route path="/buscar" element={<BuscarPokemons/>}/>
-        <Route path="/buscars" element={<BuscarPokemons/>}/>
-    </Router>
+      </Routes>
+    </HashRouter>
   )
 }
