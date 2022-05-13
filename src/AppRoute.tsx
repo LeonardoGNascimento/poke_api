f3b88152cee87b52f5ea5b86c556591fa3a0a930
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2022-05-13 19:15:30 -0300

    hashrouter

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { BuscarPokemons } from "./Pages/BuscarPokemons";
 
 export const AppRoute = () => {
